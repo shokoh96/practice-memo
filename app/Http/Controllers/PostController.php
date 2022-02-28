f1c98faa6f7dccb0a5fdc4715067b76861e00fb0
KOH <gaosheng087@gmail.com>
2022-02-28 11:47:13 +0900

    一覧表示作成

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,11 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+// use App\Post;
+use Illuminate\Support\Facades\DB;
 
 class PostController extends Controller
 {
     public function index()
     {
-        return view('posts.index');
+        // $posts = Post::all();
+        $posts = DB::select('select * from posts');
+        return view('posts.index', ['posts' => $posts]);
     }
 }
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,4 +1,33 @@
 @extends('layouts.app')
 
 @section('content')
+    <div class="container mt-5">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="mb-4">
+                    <a href="" class="btn btn-primary btn-lg">
+                        新規投稿
+                    </a>
+                </div>
+                <div class="card text-center">
+                    <div class="card-header" style="font-size: 18px;">
+                        投稿一覧
+                    </div>
+                    @foreach ($posts as $post)
+                        <div class="card-body">
+                            <h5 class="card-title">タイトル ： {{ $post->title }}</h5>
+                            <p class="card-text">
+                                内容 : {{ $post->body }}
+                            </p>
+                            <p class="card-text">投稿者： 毛さん</p>
+                            <a href="#" class="btn btn-primary">詳細へ</a>
+                        </div>
+                        <div class="card-footer text-muted">
+                            投稿日時 ： {{ $post->created_at }}
+                        </div>
+                    @endforeach
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
