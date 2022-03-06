4870c942bc91326397c008a9a78281832fc92310
KOH <gaosheng087@gmail.com>
2022-03-06 23:29:27 +0900

    コメントcreate作成

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -3,8 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Post;
 
 class CommentController extends Controller
 {
-    //
+    public function create($post_id)
+    {
+        $post = Post::find($post_id);
+
+        return view('comments.create', compact('post'));
+    }
 }
